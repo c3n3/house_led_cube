6292d892bd24cbd4a5b3f0ae6f0aaa007e0f0cd3
Caden Churchman <ccaden21@gmail.com>
2020-12-02 19:50:11 -0600

    Added stuff

--- led_cube/led_cube.ino
+++ led_cube/led_cube.ino
@@ -4,55 +4,49 @@
 #include <SPI.h>
 // extern uint8_t px_buf[NUM_LEDS / 2];
 #define LATCH_PIN 5
-#define LATCH_PIN_LAYER 6
 #define CLOCK_PIN 13
 #define DATA_PIN 11 
-#define NUM_LEDS 16
 
-#define latch_pin 2// can use any pin you want to latch the shift registers
 #define blank_pin 4// same, can use any pin you want for this, just make sure you pull up via a 1k to 5V
-#define data_pin 11// used by SPI, must be pin 11
-#define clock_pin 13// used by SPI, must be 13
 
-
-uint8_t leds0[3];
-uint8_t leds1[3];
-uint8_t leds2[3];
-uint8_t leds3[3];
+#define NUM_LEDS 64
+#define NUM_LEDS_LAYER 16
+#define NUM_LEDS_SINGLE_AXIS 4
+#define BYTES_PER_LAYER (NUM_LEDS_LAYER / 8)
+uint8_t leds0[NUM_LEDS / 8]; // each set of brightness
+uint8_t leds1[NUM_LEDS / 8]; // each set of brightness
+uint8_t leds2[NUM_LEDS / 8]; // each set of brightness
+uint8_t leds3[NUM_LEDS / 8]; // each set of brightness
 
 void drawCube() {
-    static unsigned int something = 0;
+    // delay(500);
     static uint8_t layer = 0;
     static uint8_t it = 1;
-    if (something  == -1) {
-        // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0x00);
-    digitalWrite(LATCH_PIN,1);//Latch pin HIGH
-    digitalWrite(LATCH_PIN, 0);//Latch pin LOW
-
-        // something++;
-        return;
-    }
     PORTD |= 1<<blank_pin;//The first thing we do is turn all of the LEDs OFF, by writing a 1 to the blank pin
-    // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, 0x01 << layer);
+    //  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0x01 << layer);
     SPI.transfer(0x01 << layer);
     // 8 / 15
     if (it > 7) { // 8, 9, 10, 11, 12, 13, 14, 15
-        // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds3[layer]);
-        SPI.transfer(leds3[layer]);
+        for (int i = layer * BYTES_PER_LAYER; i < layer * BYTES_PER_LAYER + BYTES_PER_LAYER; i++)
+            // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds3[i]);
+           SPI.transfer(leds3[i]);
     }
     // 4 / 15
     else if (it > 3) { // 4, 5, 6, 7
-        // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds2[layer]);
-        SPI.transfer(leds2[layer]);
+        for (int i = layer * BYTES_PER_LAYER; i < layer * BYTES_PER_LAYER + BYTES_PER_LAYER; i++)
+            // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds2[i]);
+            SPI.transfer(leds2[i]);
     }
     // 2 / 15
     else if (it > 1) { // 2, 3
-        // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds1[layer]);
-        SPI.transfer(leds1[layer]);
+        for (int i = layer * BYTES_PER_LAYER; i < layer * BYTES_PER_LAYER + BYTES_PER_LAYER; i++)
+            // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds1[i]);
+            SPI.transfer(leds1[i]);
     }
     else { // 1
-        // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds0[layer]);
-        SPI.transfer(leds0[layer]);
+        for (int i = layer * BYTES_PER_LAYER; i < layer * BYTES_PER_LAYER + BYTES_PER_LAYER; i++)
+            // shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds0[i]);
+            SPI.transfer(leds0[i]);
     }
 
     it++;
@@ -61,12 +55,10 @@ void drawCube() {
         it = 1;
     }
 
-    if (layer == 3) {
+    if (layer == NUM_LEDS_SINGLE_AXIS) {
         layer = 0;
     }
-    digitalWrite(LATCH_PIN_LAYER,1);//Latch pin HIGHf
-    digitalWrite(LATCH_PIN_LAYER, 0);//Latch pin LOW
-    // delayMicroseconds(1000);
+
     digitalWrite(LATCH_PIN,1);//Latch pin HIGHf
     digitalWrite(LATCH_PIN, 0);//Latch pin LOW
     PORTD &= 0<<blank_pin;//Blank pin LOW to turn on the LEDs with the new data
@@ -74,11 +66,13 @@ void drawCube() {
     // something++;
 }
 
-void set(uint8_t led, uint8_t layer, uint8_t power) {
-    bitWrite(leds0[layer], led, (power & 0x01));  
-    bitWrite(leds1[layer], led, (power & 0x02) >> 1);  
-    bitWrite(leds2[layer], led, (power & 0x04) >> 2);  
-    bitWrite(leds3[layer], led, (power & 0x08) >> 3);  
+void set(uint8_t x, uint8_t y, uint8_t z, uint8_t power) {
+    int whichbyte = int((z*NUM_LEDS_LAYER+y*NUM_LEDS_SINGLE_AXIS + x)/8);
+    int wholebyte = (z*NUM_LEDS_LAYER)+(y*NUM_LEDS_SINGLE_AXIS)+x;
+    bitWrite(leds3[whichbyte], wholebyte - (8 * whichbyte), (power & 0x08) >> 3); //  
+    bitWrite(leds2[whichbyte], wholebyte - (8 * whichbyte), (power & 0x04) >> 2); //  
+    bitWrite(leds1[whichbyte], wholebyte - (8 * whichbyte), (power & 0x02) >> 1); //  
+    bitWrite(leds0[whichbyte], wholebyte - (8 * whichbyte), (power & 0x01) >> 0); //  
 }
 
 /*
@@ -90,67 +84,32 @@ void setup()
     pinMode(LATCH_PIN, OUTPUT);//Latch
     pinMode(DATA_PIN, OUTPUT);//MOSI DATA
     pinMode(CLOCK_PIN, OUTPUT);//SPI Clock
-    pinMode(LATCH_PIN_LAYER, OUTPUT);//SPI Clock
-    pinMode(blank_pin, OUTPUT);//SPI Clock
-    SPI.setBitOrder(LSBFIRST);//Most Significant Bit First
-    SPI.setDataMode(SPI_MODE0);// Mode 0 Rising edge of data, keep clock low
-    SPI.setClockDivider(SPI_CLOCK_DIV2);//Run the data in at 16MHz/2 - 8MHz
-    SPI.begin();//start up the SPI library
+    pinMode(10, OUTPUT);//SPI Clock
+    pinMode(blank_pin, OUTPUT);//blank
+    for (int i = 0; i < NUM_LEDS / 8; i++) {
+        leds0[i] = 0;
+        leds1[i] = 0;
+        leds2[i] = 0;
+        leds3[i] = 0;
+    }
+    SPI.setBitOrder(LSBFIRST); //Most Significant Bit First
+    SPI.setDataMode(SPI_MODE0); // Mode 0 Rising edge of data, keep clock low
+    SPI.setClockDivider(SPI_CLOCK_DIV2); //Run the data in at 16MHz/2 - 8MHz
+    SPI.begin(); //start up the SPI library
 }
 
 /*
  * loop() - this function runs over and over again
  */
-int thing = 0;
-int dir = 0;
 uint32_t timer = millis();
-void loop() 
+void loop()
 {
-    static int row = 0;
-    static int layer = 0;
-    // if (millis() - timer > 25) {
-    //     for (int i = 0 ; i < 8; i++) {
-    //         set(i, 2, thing);
-    //         // set(i, 1, thing);
-    //         if (i % 2 == 0) {
-    //         } else {
-
-    //         }
-    //         // set(i, 0, thing);
-    //     }
-    //     thing = dir == 1 ? thing - 1 : thing + 1;
-    //     if (thing == 15) {
-    //         dir = 1;
-    //     }
-    //     if (thing == 0) {
-    //         dir = 0;
-    //     }
-    //     timer = millis();
-    // }
-    // set(1, 1, 15);
-    // set(2, 2, 15);
-    // set(3, 0, 15);
-    // set(1, 1, 15);
-    // if (millis() - timer > 100) {
-    //     set(row, layer, 15);
-    //     set((((row + 8) - 1) % 8), layer, 0);
-    //     timer = millis();
-    //     row++;
-    //     if (row == 8) {
-    //         row = 0;
-    //         layer = layer == 2 ? 0 : layer + 1;
-    //     }
-    // }
-    for (int i = 0; i < 8 ; i++) {
-        // set(i, 0, 15);
-        set(i, 1, 15);
+    for (int i= 0; i< NUM_LEDS_SINGLE_AXIS; i++) {
+        for (int k= 0; k< NUM_LEDS_SINGLE_AXIS; k++) {
+            for (int j= 0; j< NUM_LEDS_SINGLE_AXIS; j++) {
+                set(i, j, k, 1);
+            }
+        }
     }
-    // set(1, 0, 15);
-    // set(1, 1, 7);
-    // set(2, 0, 1);
-    // set(3, 1, 7);
-    // set(4, 1, 15);
-    // delay(200);
-    // delay(200);
     drawCube();
 }
